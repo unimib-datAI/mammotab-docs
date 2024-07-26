24c135f2297e313eb31b4c18b9f528c34f145964
Marco <marco.cremaschi@unimib.it>
2024-07-26 16:23:42 +0200

    First commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,99 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-plugin.cjs';
+
+const config: Config = {
+  title: 'MammoTab',
+  tagline: 'Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://unimibinside.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/mammotab-docs/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'DatAI Lab', // Usually your GitHub org/user name.
+  projectName: 'MammoTab', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '1.0.0',
+              badge :true,
+            },
+          },
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/mantistable-ui-social-card.png',
+    navbar: {
+      title: 'MammoTab',
+      logo: {
+        alt: 'MammoTab',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/UNIMIBInside/mantistable-ui',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ]
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} DatAI lab. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    colorMode: {
+      defaultMode: 'dark'
+    },
+  } satisfies Preset.ThemeConfig,
+
+  // Tailwind
+  plugins: [tailwindPlugin],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="MammoTab.">
+      <main>
+        <div>
+          QUI PUOI INSERIRE HTML E TAILWIND
+          <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Badge</span>
+        </div>
+      </main>
+    </Layout>
+  );
+}
